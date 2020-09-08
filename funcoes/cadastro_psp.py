d798338817841f000c83c818cfd3a4b21d7797d9
gorpoorko <gorpoorko@protonmail.com>
2020-09-08 14:30:34 -0300

    inseridos .setToolTip('') na home

--- funcoes/cadastro_psp.py
+++ funcoes/cadastro_psp.py
@@ -56,7 +56,6 @@ def selecionarImagempsp(self):
         imagem_recebida = Image.open(self.imagem_recebida[0])
         # salva a imagem usando a extensao que quisermos
         #imagem_recebida = imagem_recebida.save("images/file.jpg")
-
         #TRATAMENTO DA IMAGEM RECEBIDA PONDO MARCA DAGUA E REDIMENSIONANDO
         # imagem de entrada e marca dagua
         #imagem_entrada = Image.open(imagem_recebida)
@@ -76,12 +75,7 @@ def selecionarImagempsp(self):
         # exibe a imagem
         # imagem_final.show()
         self.ui.imagem_psp.setPixmap(QtGui.QPixmap(f'images/{self.nome_imagem}').scaled(250, 250, QtCore.Qt.KeepAspectRatio))
-
         #CONEXAO COM FTP PARA UPLOAD DA imagem
-        # conecta ao ftp | força UTF-8 encoding
-        #ftp = ftplib.FTP(host="192.168.0.3", user="gorpo", passwd="")
-        #ftp.encoding = "utf-8"
-
         # arquivo para ser enviado ao server
         file = open(f'images/{self.nome_imagem}', 'rb')  # file to send
         conexao.ftp.storbinary(f'STOR assets/images/psp/{self.nome_imagem}', file)  # send the file
@@ -90,8 +84,6 @@ def selecionarImagempsp(self):
         os.remove(f'images/{self.nome_imagem}')
     except:
         pass
-
-
 
 
 
@@ -115,7 +107,7 @@ def bancoDadospsp(self):
     self.i_psp = self.model_psp.rowCount()
 
 
-
+#ADICIONA CHAVES
 def addToDbpsp(self):
     #chama a função de conexao e popula a tabela | verificar com  print(self.i)
     bancoDadospsp(self)
@@ -131,23 +123,27 @@ def addToDbpsp(self):
     else:
 
         try:
-            # hora e data para ser insidas no servidor
-            self.hoje = datetime.now()
-            self.data_formatada = self.hoje.strftime('%Y-%m-%d %H:%M:%S')
-            # insere os dados na database
-            self.model_psp.insertRows(self.i_psp, 1)
-            self.model_psp.setData(self.model_psp.index(self.i_psp, 1), self.ui.input_titulo_jogo_psp.text())     # TITULO
-            self.model_psp.setData(self.model_psp.index(self.i_psp, 2), self.ui.input_descricao_jogo_psp.text())  # DESCRIÇÃO
-            self.model_psp.setData(self.model_psp.index(self.i_psp, 3), self.ui.input_contentid_psp.text())       # CONTENT_ID
-            self.model_psp.setData(self.model_psp.index(self.i_psp, 4), self.nome_imagem)                         # IMAGEM
-            self.model_psp.setData(self.model_psp.index(self.i_psp, 5), self.data_formatada)                      # CADASTRO
-            self.model_psp.setData(self.model_psp.index(self.i_psp, 6), self.ui.input_link_jogo_psp.text())       # LINK
-            self.model_psp.submitAll()
-            self.i_psp += 1
-        except Exception as e:
+            self.nome_imagem = self.nome_imagem
+            if self.nome_imagem:
+                # hora e data para ser insidas no servidor
+                self.hoje = datetime.now()
+                self.data_formatada = self.hoje.strftime('%Y-%m-%d %H:%M:%S')
+                # insere os dados na database
+                self.model_psp.insertRows(self.i_psp, 1)
+                self.model_psp.setData(self.model_psp.index(self.i_psp, 1), self.ui.input_titulo_jogo_psp.text())     # TITULO
+                self.model_psp.setData(self.model_psp.index(self.i_psp, 2), self.ui.input_descricao_jogo_psp.text())  # DESCRIÇÃO
+                self.model_psp.setData(self.model_psp.index(self.i_psp, 3), self.ui.input_contentid_psp.text())       # CONTENT_ID
+                self.model_psp.setData(self.model_psp.index(self.i_psp, 4), self.nome_imagem)                         # IMAGEM
+                self.model_psp.setData(self.model_psp.index(self.i_psp, 5), self.data_formatada)                      # CADASTRO
+                self.model_psp.setData(self.model_psp.index(self.i_psp, 6), self.ui.input_link_jogo_psp.text())       # LINK
+                self.model_psp.submitAll()
+                self.i_psp += 1
+        except AttributeError:
+            QMessageBox.question(self, 'TCXS Project | AVISO!', """Faça o envio da imagem antes de adicionar!""", QMessageBox.Ok)
+            self.show()
             pass
 
-
+#ATUALIZA AS CHAVES
 def updaterowpsp(self):
     if self.ui.tabela_dados_db_psp.currentIndex().row() > -1:
         # hora e data para ser insidas no servidor
@@ -163,21 +159,17 @@ def updaterowpsp(self):
         record.setValue("link", self.ui.input_descricao_jogo_psp.text())      # LINK
         self.model_psp.setRecord(self.ui.tabela_dados_db_psp.currentIndex().row(), record)
     else:
-        QMessageBox.question(self, 'Mensagem',
-                             "Selecione uma linha para atualizar, clique sobre o numero a esquerda na tabela correspondente a linha.",
-                             QMessageBox.Ok)
+        QMessageBox.question(self, 'TCXS Project | Atualizar PSP', "Selecione uma linha para atualizar, clique sobre o numero a esquerda na tabela correspondente a linha.", QMessageBox.Ok)
         self.show()
 
 
 
-
+#DELETA AS CHAVES
 def delrowpsp(self):
     if self.ui.tabela_dados_db_psp.currentIndex().row() > -1:
         self.model_psp.removeRow(self.ui.tabela_dados_db_psp.currentIndex().row())
         self.i_psp -= 1
         self.model_psp.select()
     else:
-        QMessageBox.question(self, 'Mensagem',
-                             "Selecione uma linha para deletar, clique sobre o numero a esquerda na tabela correspondente a linha.",
-                             QMessageBox.Ok)
+        QMessageBox.question(self, 'TCXS Project | Deletar PSP',"Selecione uma linha para deletar, clique sobre o numero a esquerda na tabela correspondente a linha.", QMessageBox.Ok)
         self.show()
--- funcoes/home.py
+++ funcoes/home.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#███╗   ███╗ █████╗ ███╗   ██╗██╗ ██████╗ ██████╗ ███╗   ███╗██╗ ██████╗
-#████╗ ████║██╔══██╗████╗  ██║██║██╔════╝██╔═══██╗████╗ ████║██║██╔═══██╗
-#██╔████╔██║███████║██╔██╗ ██║██║██║     ██║   ██║██╔████╔██║██║██║   ██║
-#██║╚██╔╝██║██╔══██║██║╚██╗██║██║██║     ██║   ██║██║╚██╔╝██║██║██║   ██║
-#██║ ╚═╝ ██║██║  ██║██║ ╚████║██║╚██████╗╚██████╔╝██║ ╚═╝ ██║██║╚██████╔╝
-#╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝ ╚═════╝
+# ███╗   ███╗ █████╗ ███╗   ██╗██╗ ██████╗ ██████╗ ███╗   ███╗██╗ ██████╗
+# ████╗ ████║██╔══██╗████╗  ██║██║██╔════╝██╔═══██╗████╗ ████║██║██╔═══██╗
+# ██╔████╔██║███████║██╔██╗ ██║██║██║     ██║   ██║██╔████╔██║██║██║   ██║
+# ██║╚██╔╝██║██╔══██║██║╚██╗██║██║██║     ██║   ██║██║╚██╔╝██║██║██║   ██║
+# ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║╚██████╗╚██████╔╝██║ ╚═╝ ██║██║╚██████╔╝
+# ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝ ╚═════╝
 #            @GorpoOrko | Manicomio TCXS Project | 2020
+
 from main import *
 import sqlite3
-
+import sys
+import os
 
 
 def funcoesHome(self):
     exibeDadosMysql(self)
-    #eventos para limpar os campos, que buscam funçoes no arquivo main
+    # eventos para limpar os campos, que buscam funçoes no arquivo main
     self.ui.input_nome_database_home.mousePressEvent = self.limpaNomedb
+    self.ui.input_nome_database_home.setToolTip('Insira o nome da Database Mysql')
     self.ui.input_user_database_home.mousePressEvent = self.limpaUserdb
+    self.ui.input_user_database_home.setToolTip('Insira o usuário da Database Mysql')
     self.ui.input_senha_database_home.mousePressEvent = self.limpaSenhadb
+    self.ui.input_senha_database_home.setToolTip('Insira a senha da Database Mysql')
     self.ui.input_host_database_home.mousePressEvent = self.limpaHostdb
+    self.ui.input_host_database_home.setToolTip('Insira o hostname ou ip da Database Mysql')
     self.ui.input_endereco_ftp.mousePressEvent = self.limpaEnderecoFtp
+    self.ui.input_endereco_ftp.setToolTip('Insira o endereço do Ftp')
     self.ui.input_user_ftp.mousePressEvent = self.limpaUserFtp
+    self.ui.input_user_ftp.setToolTip('Insira o usuário do Ftp')
     self.ui.input_senha_ftp.mousePressEvent = self.limpaSenhaFtp
+    self.ui.input_senha_ftp.setToolTip('Insira a senha do Ftp')
     self.ui.btn_database_home.clicked.connect(lambda: addDadosMysqlToDb(self))
 
 
-
 def exibeDadosMysql(self):
-
     try:
         self.conexao = sqlite3.connect('database.db')
         self.conexao.row_factory = sqlite3.Row
@@ -53,8 +60,6 @@ def exibeDadosMysql(self):
         pass
 
 
-
-
 def addDadosMysqlToDb(self):
     try:
         self.conexao = sqlite3.connect('database.db')
@@ -76,7 +81,7 @@ def addDadosMysqlToDb(self):
     self.host_database = self.ui.input_host_database_home.text()
     self.endereco_ftp = self.ui.input_endereco_ftp.text()
     self.user_ftp = self.ui.input_user_ftp.text()
-    self.senha_ftp =  self.ui.input_senha_ftp.text()
+    self.senha_ftp = self.ui.input_senha_ftp.text()
     self.cursor.execute(f""" INSERT INTO  dados_mysql ( nome_database, user_database, senha_database, host_database, 
 endereco_ftp, user_ftp, senha_ftp) VALUES ( '{self.nome_database}', '{self.user_database}', '{self.senha_database}', 
 '{self.host_database}', '{self.endereco_ftp}', '{self.user_ftp}', '{self.senha_ftp}')""")
@@ -90,4 +95,29 @@ endereco_ftp, user_ftp, senha_ftp) VALUES ( '{self.nome_database}', '{self.user_
     self.ui.input_user_ftp.setText('Usuário FTP salvo')
     self.ui.input_senha_ftp.setText('Senha FTP salva')
 
+    #aviso:
+    QMessageBox.question(self, 'TCXS Project | Cadastro de dados MySql FTP', f"""Bem vindo ao Programa de Administração da TCXS Project, obrigado por fornecer os dados necessários para uso do programa, confira as informações armazenadas, em caso de erros das informações as funções do programa não iráo funcionar, você terá certeza que inseriu as informações certas ao usar os menus e ver que ocorreu sua conexão com exito.
 
+[Como usar após dados inseridos]
+Após ter inserido os dados para conexão MySql e Ftp basta ir nos menus que você quer editar e clicar sobre os botões adicionar para ativar o sistema daquele menu e receber as instruções dele!
+
+[Dados Mysql cadastrados]
+    Nome database:    {self.nome_database}
+    User database:    {self.user_database}
+    Senha database:   {self.senha_database}
+    Host database:    {self.host_database}
+
+[Dados Ftp Cadastrados]
+    Endereço Ftp:     {self.endereco_ftp}
+    User Ftp:         {self.user_ftp}
+    Senha Ftp:        {self.senha_ftp}
+
+AVISO: O programa será reiniciado com os novos dados!
+Quando o programa for reiniciado já pode ser usado os menus, seus dados ficarão salvos até que os altere novamente, somente use este campo quando for atualizar os dados do servidor no programa, caso ja tenah seus dados salvos apenas abra o programa e edite oque quiser. Vamos reiniciar o programa agora, por favor aguarde!
+""",QMessageBox.Ok)
+    self.show()
+
+
+    #reinicia o programa caso salvo novos dados na database_Sqlite Local
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
